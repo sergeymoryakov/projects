9f537f3b3c2459e06512c49538f0a01687f42aae
Sergey Moryakov <sergey@moryakov.com>
2023-08-07 20:52:55 +0300

    Add gitignore, js, data.js and styling

---
+++ data.js
@@ -0,0 +1,73 @@
+export const projectsData = [
+    {
+        name: "CV",
+        description: "A personal resume.",
+        link: "https://sergeymoryakov.github.io/cv",
+        important: true,
+    },
+    {
+        name: "Seppo.Digital",
+        description:
+            "A landing page of Seppo.Digital - a web development studio.",
+        link: "https://sergeymoryakov.github.io/seppo-digital",
+        important: true,
+    },
+    {
+        name: "Metrica",
+        description: "A landing page of Metrica - an interiors design studio.",
+        link: "https://sergeymoryakov.github.io/metrica",
+        important: true,
+    },
+    {
+        name: "Counter",
+        description: "A simple counter application to practice JavaScript.",
+        link: "https://sergeymoryakov.github.io/counter",
+        important: false,
+    },
+    {
+        name: "Expense Tracker",
+        description: "A simple personal expense tracker application.",
+        link: "https://sergeymoryakov.github.io/expense-tracker",
+        important: true,
+    },
+    {
+        name: "Action Items",
+        description:
+            "A personal application to create and track personal action items list.",
+        link: "https://sergeymoryakov.github.io/action-items",
+        important: false,
+    },
+    {
+        name: "Blog",
+        description:
+            "A blog drafting and posting application to fit into any website.",
+        link: "https://sergeymoryakov.github.io/blog",
+        important: true,
+    },
+    {
+        name: "Bored",
+        description: "A funny application to practice JavaScript",
+        link: "https://sergeymoryakov.github.io/bored",
+        important: false,
+    },
+    {
+        name: "Action Items (Firebase)",
+        description:
+            "A personal application to create and track personal action items list. Practicing MVC pattern and Google Firebase backend.",
+        link: "https://sergeymoryakov.github.io/action-items-frb/",
+        important: true,
+    },
+    {
+        name: "Movies",
+        description:
+            "An application to search movies, create and track personal to-watch list.",
+        link: "https://sergeymoryakov.github.io/movies",
+        important: true,
+    },
+    {
+        name: "Under Construction",
+        description: "An Under Constrcution landing page.",
+        link: "https://sergeymoryakov.github.io/under-construction",
+        important: false,
+    },
+];
